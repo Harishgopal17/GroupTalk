d0a051689dfd36b660a23b0e59ea77efe05e75fe
Harishgopal17 <harish.b.g@cli.in>
2025-01-22 09:35:10 +0530

    small change

--- client/src/components/NewRoom.jsx
+++ client/src/components/NewRoom.jsx
@@ -29,7 +29,7 @@ export default function NewRoom({
 
   return (
     <div className="fixed top-0 left-0 w-full h-screen bg-black bg-opacity-30 flex items-center justify-center z-50">
-      <div className="bg-[#e9ecef] rounded-xl shadow-lg p-5 w-[90%] max-w-sm text-center flex flex-col">
+      <div className="bg-[#e9ecef] rounded-xl shadow-lg p-5 max-w-md text-center flex flex-col">
         <div className="flex flex-col gap-4 justify-center">
           <div className="flex justify-center gap-5">
             <h6 className="text-2xl font-medium text-start max-md:text-xl">
@@ -51,7 +51,7 @@ export default function NewRoom({
               save it so that you can use is later, too.
             </p>
           </div>
-          <div className="flex items-center justify-between bg-[#dde3ea] px-6 py-1 w-full rounded-lg leading-5 max-md:px-2 max-md:text-base">
+          <div className="flex items-center justify-between bg-[#dde3ea] px-3 py-1 w-full rounded-lg leading-5 max-md:px-2 max-md:text-base">
             <p>{`${FRONTEND_URI}/${roomId}`}</p>
             {iscopied ? (
               <span className="cursor-pointer rounded-full self-center p-2 hover:bg-[#cbcfd3] transition-colors duration-300">
